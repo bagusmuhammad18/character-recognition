3f0a7418e4ef94e240aaec61c06ad7c7e035054f
Bagus Muhammad <98868039+bagusmuhammad18@users.noreply.github.com>
2025-04-08 14:41:59 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,129 +1,49 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F  # Untuk operasi pooling
-import matplotlib.pyplot as plt
-import cv2  # Pastikan OpenCV terinstal: pip install opencv-python
-import os  # Tambahkan impor ini untuk os.makedirs
-from modules.transformation import TPS_SpatialTransformerNetwork
-from modules.feature_extraction import VGG_FeatureExtractor, RCNN_FeatureExtractor, ResNet_FeatureExtractor
-from modules.sequence_modeling import BidirectionalLSTM
-from modules.prediction import Attention
+def test(opt):
+    if 'CTC' in opt.Prediction:
+        converter = CTCLabelConverter(opt.character)
+    else:
+        converter = AttnLabelConverter(opt.character)
+    opt.num_class = len(converter.character)
 
-class Model(nn.Module):
+    if opt.rgb:
+        opt.input_channel = 3
+    model = Model(opt)
+    print('model input parameters', opt.imgH, opt.imgW, opt.num_fiducial, opt.input_channel, opt.output_channel,
+          opt.hidden_size, opt.num_class, opt.batch_max_length, opt.Transformation, opt.FeatureExtraction,
+          opt.SequenceModeling, opt.Prediction)
+    model = torch.nn.DataParallel(model).to(device)
 
-    def __init__(self, opt):
-        super(Model, self).__init__()
-        self.opt = opt
-        self.stages = {'Trans': opt.Transformation, 'Feat': opt.FeatureExtraction,
-                       'Seq': opt.SequenceModeling, 'Pred': opt.Prediction}
-        self.image_counter = 0  # Counter untuk menyimpan gambar thresholded dan morphology
-        
-        # Buat folder threshold dan morphology jika belum ada
-        os.makedirs('threshold', exist_ok=True)
-        os.makedirs('morphology', exist_ok=True)
+    print('loading pretrained model from %s' % opt.saved_model)
+    model.load_state_dict(torch.load(opt.saved_model, map_location=device, weights_only=True))
+    opt.exp_name = '_'.join(opt.saved_model.split('/')[1:])
 
-        """ Transformation """
-        if opt.Transformation == 'TPS':
-            self.Transformation = TPS_SpatialTransformerNetwork(
-                F=opt.num_fiducial, 
-                I_size=(opt.imgH, opt.imgW), 
-                I_r_size=(opt.imgH, opt.imgW), 
-                I_channel_num=opt.input_channel
-            )
+    os.makedirs(f'./result/{opt.exp_name}', exist_ok=True)
+    os.system(f'cp {opt.saved_model} ./result/{opt.exp_name}/')
+
+    if 'CTC' in opt.Prediction:
+        criterion = torch.nn.CTCLoss(zero_infinity=True).to(device)
+    else:
+        criterion = torch.nn.CrossEntropyLoss(ignore_index=0).to(device)
+
+    model.eval()
+    with torch.no_grad():
+        if opt.benchmark_all_eval:
+            benchmark_all_eval(model, criterion, converter, opt)
         else:
-            print('No Transformation module specified')
+            log = open(f'./result/{opt.exp_name}/log_evaluation.txt', 'a')
+            AlignCollate_evaluation = AlignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio_with_pad=opt.PAD)
+            eval_data, eval_data_log = hierarchical_dataset(root=opt.eval_data, opt=opt)
+            evaluation_loader = torch.utils.data.DataLoader(
+                eval_data, batch_size=opt.batch_size,
+                shuffle=False,
+                num_workers=int(opt.workers),
+                collate_fn=AlignCollate_evaluation, pin_memory=True)
+            _, accuracy_by_best_model, _, _, _, _, _, _ = validation(
+                model, criterion, evaluation_loader, converter, opt)
+            log.write(eval_data_log)
+            print(f'{accuracy_by_best_model:0.3f}')
+            log.write(f'{accuracy_by_best_model:0.3f}\n')
+            log.close()
 
-        """ Feature Extraction """
-        if opt.FeatureExtraction == 'VGG':
-            self.FeatureExtraction = VGG_FeatureExtractor(opt.input_channel, opt.output_channel)
-        elif opt.FeatureExtraction == 'RCNN':
-            self.FeatureExtraction = RCNN_FeatureExtractor(opt.input_channel, opt.output_channel)
-        elif opt.FeatureExtraction == 'ResNet':
-            self.FeatureExtraction = ResNet_FeatureExtractor(opt.input_channel, opt.output_channel)
-        else:
-            raise Exception('No FeatureExtraction module specified')
-        self.FeatureExtraction_output = opt.output_channel
-        self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))
-
-        """ Sequence Modeling """
-        if opt.SequenceModeling == 'BiLSTM':
-            self.SequenceModeling = nn.Sequential(
-                BidirectionalLSTM(self.FeatureExtraction_output, opt.hidden_size, opt.hidden_size),
-                BidirectionalLSTM(opt.hidden_size, opt.hidden_size, opt.hidden_size)
-            )
-            self.SequenceModeling_output = opt.hidden_size
-        else:
-            print('No SequenceModeling module specified')
-            self.SequenceModeling_output = self.FeatureExtraction_output
-
-        """ Prediction """
-        if opt.Prediction == 'CTC':
-            self.Prediction = nn.Linear(self.SequenceModeling_output, opt.num_class)
-        elif opt.Prediction == 'Attn':
-            self.Prediction = Attention(self.SequenceModeling_output, opt.hidden_size, opt.num_class)
-        else:
-            raise Exception('Prediction is neither CTC or Attn')
-
-    def forward(self, input, text, is_train=True):
-        """ Transformation stage """
-        if not self.stages['Trans'] == "None":
-            input = self.Transformation(input)
-
-        """ Preprocessing stage: Grayscale dan Binarisasi dengan Adaptive Thresholding berbasis Local Mean (Dikomentari kecuali Grayscale) """
-        # Jika input memiliki 3 channel (RGB), konversi menjadi grayscale
-        if input.size(1) == 3:  # input shape: (batch_size, channels, height, width)
-            processed = 0.2989 * input[:, 0:1, :, :] + 0.5870 * input[:, 1:2, :, :] + 0.1140 * input[:, 2:3, :, :]
-        else:
-            processed = input  # Jika sudah grayscale (1 channel), gunakan langsung
-
-        # # Binarisasi: Gunakan adaptive thresholding berbasis local mean
-        # kernel_size = 15  # Ukuran kernel untuk menghitung rata-rata lokal, bisa disesuaikan
-        # padding = kernel_size // 2  # Padding agar ukuran output tidak berubah
-        # local_mean = F.avg_pool2d(processed, kernel_size, stride=1, padding=padding)  # Hitung rata-rata lokal
-        # offset = 0.05  # Offset untuk menyesuaikan threshold, dapat disesuaikan
-        # processed = (processed > (local_mean - offset)).float()  # Terapkan thresholding, hasilnya 0 atau 1
-
-        # # Simpan hasil thresholding (hanya batch pertama untuk efisiensi)
-        # if self.image_counter < 100:  # Batasi penyimpanan maksimal 100 gambar
-        #     img_to_save = processed[0].squeeze().cpu().numpy()  # Ambil gambar pertama dari batch
-        #     plt.imsave(f'threshold/threshold_{self.image_counter:03d}.png', img_to_save, cmap='gray')
-
-        """ Operasi Morfologi: Opening untuk Pengenalan Karakter Plat Nomor (Dikomentari) """
-        # # Erosi: Menggunakan max pooling pada inversi gambar untuk mengecilkan area putih
-        # eroded = 1 - F.max_pool2d(1 - processed, kernel_size=3, stride=1, padding=1)
-        # # Dilasi: Menggunakan max pooling untuk mengembalikan ukuran karakter
-        # processed = F.max_pool2d(eroded, kernel_size=3, stride=1, padding=1)
-
-        # # Simpan hasil operasi morfologi (hanya batch pertama untuk efisiensi)
-        # if self.image_counter < 100:  # Batasi penyimpanan maksimal 100 gambar
-        #     img_morph_to_save = processed[0].squeeze().cpu().numpy()  # Ambil gambar pertama dari batch
-        #     plt.imsave(f'morphology/morphology_{self.image_counter:03d}.png', img_morph_to_save, cmap='gray')
-        #     self.image_counter += 1  # Increment counter setelah menyimpan kedua gambar
-
-        # # (Operasi lain tetap dikomentari untuk referensi)
-        # # dilated = F.max_pool2d(processed, kernel_size=3, stride=1, padding=1)  # Dilasi
-        # # closed = 1 - F.max_pool2d(1 - dilated, kernel_size=3, stride=1, padding=1)  # Closing
-
-        """ Feature extraction stage """
-        visual_feature = self.FeatureExtraction(processed)  # Gunakan processed setelah grayscale
-        visual_feature = self.AdaptiveAvgPool(visual_feature.permute(0, 3, 1, 2))
-        visual_feature = visual_feature.squeeze(3)
-
-        """ Sequence modeling stage """
-        if self.stages['Seq'] == 'BiLSTM':
-            contextual_feature = self.SequenceModeling(visual_feature)
-        else:
-            contextual_feature = visual_feature
-
-        """ Prediction stage """
-        if self.stages['Pred'] == 'CTC':
-            prediction = self.Prediction(contextual_feature.contiguous())
-        else:
-            prediction = self.Prediction(
-                contextual_feature.contiguous(), 
-                text, 
-                is_train, 
-                batch_max_length=self.opt.batch_max_length
-            )
-
-        return prediction
+    # Panggil fungsi untuk zip gambar setelah evaluasi selesai
+    model.module.zip_preprocessed_images()  # Karena model dibungkus dengan DataParallel, gunakan .module
